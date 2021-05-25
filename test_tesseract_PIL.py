53021e0e143a6be0d037b74fa81a23992b8c8dee
D076 <33459398+D076@users.noreply.github.com>
2021-05-25 13:36:14 +0700

    Добавлено логирование в файл logs.log

--- test_tesseract_PIL.py
+++ test_tesseract_PIL.py
@@ -2,6 +2,8 @@ import pytesseract as tsrct
 from PIL import Image
 import os
 import shutil
+import datetime
+from pathlib import Path
 
 # Настройки
 # Путь к установленному tesseract
@@ -16,6 +18,24 @@ spec_symbols = ['°', '`', '‘', '*', '^', '#', '@', '.', '"', "'",
                 '%', ':', ';', '$', '№', '~', '=', '+', '(', ')',
                 '{', '}']
 nomenclature = None
+
+
+def save_logging(conf=None, str=None):
+    if conf is None:
+        conf = {}
+    with open('logs.log', 'a') as f:
+        if str is None:
+            f.write(f'\n' + '*'*50 + '\n')
+            now = datetime.datetime.now()
+            f.write(f'{now.strftime("%d-%m-%Y %H:%M:%S")}\n')
+            if conf != {}:
+                f.write(f"tesseract_path={conf['tesseract_path']}\n")
+                f.write(f"folder_with_maps={conf['folder_with_maps']}\n")
+                f.write(f"target_folder={conf['target_folder']}\n")
+                f.write(f"height_of_head={conf['height_of_head']}\n\n")
+        else:
+            f.write(f'{str}\n')
+    return
 
 
 def get_images_from_dir(folder='maps'):
@@ -41,6 +61,10 @@ def get_nomenclature(data_string):
     """
     # Пытаемся найти слова, схожие с номенклатурой
     data = data_string.split()
+    try:
+        save_logging(str=f'Data from image {data}')
+    except Exception:
+        pass
     potential_nomenclature = []
     prev_word = data[0]
     for word in data:
@@ -57,6 +81,7 @@ def get_nomenclature(data_string):
         # print(word)
 
     # Если слов напоминающих номенклатуру не удалось обнаружить, возвращаем None
+    save_logging(str=f'Potential nomenclatures: {potential_nomenclature}')
     if len(potential_nomenclature) == 0:
         return None
         # print(f'Номенклатура не найдена!')
@@ -87,6 +112,8 @@ def init(conf=None):
     """
     if conf is None:
         conf = {}
+    save_logging(conf=conf)
+
     tsrct.pytesseract.tesseract_cmd = conf['tesseract_path']
     path_of_maps = get_images_from_dir(conf['folder_with_maps'])
     target_folder = conf['target_folder']
@@ -94,7 +121,6 @@ def init(conf=None):
     lang = conf['lang']
     for path in path_of_maps:
         # Подключение фото
-        # img = Image.open(path_of_maps[0])
         img = Image.open(path)
         # Вычислим размер фото и шапки
         width, height = img.size
@@ -107,31 +133,34 @@ def init(conf=None):
         # Лучше использовать rus+eng
         data_string = tsrct.image_to_string(img, lang=lang, config=config)
         # Листинг
-        # print(data_string)
+        save_logging(str=f'{path_of_maps.index(path) + 1} / {len(path_of_maps)} | '
+                         f'{((path_of_maps.index(path) + 1) / (len(path_of_maps)) * 100)}%')
+        save_logging(str=f'{path}')
         print(f'*'*50)
         print(f'{path_of_maps.index(path) + 1} / {len(path_of_maps)} | '
               f'{((path_of_maps.index(path) + 1) / (len(path_of_maps)) * 100)}%')
         print(f'{path}')
-        nomenclature = get_nomenclature(data_string)
+        nomenclature = None
+        try:
+            nomenclature = get_nomenclature(data_string)
+        except Exception:
+            pass
         print(nomenclature)
+        save_logging(str=f'Nomenclature {nomenclature}')
 
-        shutil.copy(path, target_folder)
+        rashirenie = Path(path).suffix
+        file_name = ''
+        if nomenclature is None:
+            file_name = f'{path_of_maps.index(path) + 1}-{len(path_of_maps)}'
+        else:
+            file_name = f'{nomenclature}'
+        i = 0
+        while os.path.exists(target_folder+f'/{file_name}{rashirenie}'):
+            i += 1
+            if not os.path.exists(target_folder + f'/{file_name}({i}){rashirenie}'):
+                file_name += f'({i})'
 
-    # # Подключение фото
-    # img = Image.open(path_of_maps[3])
-    # # Вычислим размер фото и шапки
-    # width, height = img.size
-    # height_head = int(height * height_of_head / 100)
-    # # Вырезаем шапку, в которой находится номенклатура
-    # img = img.crop((0, 0, width, height_head))
-    # # img.show()
-    #
-    # # Определяем текст на изображении
-    # # Лучше использовать rus+eng
-    # data_string = tsrct.image_to_string(img, lang='rus+eng', config=config)
-    # print(data_string)
-    # print(f'*'*50)
-    # nomenclature = get_nomenclature(img, data_string)
-    # # print(f'{path}')
-    # print(nomenclature)
+        shutil.copy(path, target_folder+f'/{file_name}{rashirenie}')
+        save_logging(str=f'Saved as {target_folder}/{file_name}{rashirenie}\n')
+
     return
