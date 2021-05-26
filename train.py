748ab6aa1024fe3deb2b6c129980b57d0ce6b68e
D076 <33459398+D076@users.noreply.github.com>
2021-05-26 16:07:17 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,22 +1,29 @@
-replace_dict = {'~': '-',
-                '!': '1',
-                'l': '1',
-                '—': '-',
-                'У': 'V',
-                'Ш': 'III'}
+# replace_dict = {'~': '-',
+#                 '!': '1',
+#                 'l': '1',
+#                 '—': '-',
+#                 'У': 'V',
+#                 'Ш': 'III'}
+#
+# potential_nomenclature = ['l4-44-107', 'l4-44-УШ']
+# print(potential_nomenclature)
+# new_potential_nomenclature = []
+# for word in potential_nomenclature:
+#     for i in replace_dict:
+#         print(word)
+#         print(i)
+#         j = replace_dict.get(i)
+#         print(j)
+#         word = word.replace(i, j)
+#         print(word)
+#     new_potential_nomenclature.append(word)
+#
+# potential_nomenclature = new_potential_nomenclature
+# print(potential_nomenclature)
+# word = '1-2-3-4'
+# print(word.split('-')[6])
+a = []
+for i in range(61):
+    a.append(str(i))
 
-potential_nomenclature = ['l4-44-107', 'l4-44-УШ']
-print(potential_nomenclature)
-new_potential_nomenclature = []
-for word in potential_nomenclature:
-    for i in replace_dict:
-        print(word)
-        print(i)
-        j = replace_dict.get(i)
-        print(j)
-        word = word.replace(i, j)
-        print(word)
-    new_potential_nomenclature.append(word)
-
-potential_nomenclature = new_potential_nomenclature
-print(potential_nomenclature)
+print(a)
