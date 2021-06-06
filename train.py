0fa8b4db29ccc6e4ef72a28b2944ed227aee65c0
D076 <33459398+D076@users.noreply.github.com>
2021-06-06 20:24:46 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,20 +1,27 @@
+
 replace_dict = {'~': '-',
                 '!': '1',
                 'l': '1',
                 '—': '-',
                 'У': 'V',
-                'Ш': 'III'}
+                'Ш': 'III',
+                '#': 'N'}
 spec_symbols = ['°', '`', '‘', '*', '^', '#', '@', '.', '"', "'",
                 '%', ':', ';', '$', '№', '~', '=', '+', '(', ')',
                 '{', '}', '[', ']']
+# first_letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G',
+#                 'H', 'I', 'G', 'K', 'L', 'M', 'N',
+#                 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+#                 'V', 'Z'
+#                 'А', 'М', 'О', 'Т', 'Е', 'К', 'Н',
+#                 '0', '1', '2', '3', '4', '5', '6',
+#                 '7', '8', '9', '10', '11', '12', '13',
+#                 '14', '15', '16', '17', '18', '19', '20', '21']
 first_letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G',
                 'H', 'I', 'G', 'K', 'L', 'M', 'N',
                 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
                 'V', 'Z'
-                'А', 'М', 'О', 'Т', 'Е', 'К', 'Н',
-                '0', '1', '2', '3', '4', '5', '6',
-                '7', '8', '9', '10', '11', '12', '13',
-                '14', '15', '16', '17', '18', '19', '20', '21']
+                'А', 'М', 'О', 'Т', 'Е', 'К', 'Н', 'В', 'С', 'Р']
 second_letter = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
                 '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
                 '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
@@ -50,81 +57,19 @@ third_letter = ['А', 'Б', 'В', 'Г', '1', '2', '3', '4', 'A',
 fourth_letter = ['A', 'Б', 'В', 'Г', 'A']
 fifth_letter = ['а', 'б', 'в', 'г', 'a']
 nomenclature = None
-# replace_dict = {'~': '-',
-#                 '!': '1',
-#                 'l': '1',
-#                 '—': '-',
-#                 'У': 'V',
-#                 'Ш': 'III'}
-#
-# potential_nomenclature = ['l4-44-107', 'l4-44-УШ']
-# print(potential_nomenclature)
-# new_potential_nomenclature = []
-# for word in potential_nomenclature:
-#     for i in replace_dict:
-#         print(word)
-#         print(i)
-#         j = replace_dict.get(i)
-#         print(j)
-#         word = word.replace(i, j)
-#         print(word)
-#     new_potential_nomenclature.append(word)
-#
-# potential_nomenclature = new_potential_nomenclature
-# print(potential_nomenclature)
-# word = '1-2-3-4'
-# print(word.split('-')[6])
-# a = []
-# for i in range(10, 100):
-#     s = str(i)
-#
-#     a.append(s)
 
-# def checkio(n):
-#     result = ''
-#     for arabic, roman in zip((1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1),
-#                              'M     CM   D    CD   C    XC  L   XL  X   IX V  IV I'.split()):
-#         result += n // arabic * roman
-#         n %= arabic
-#         # print('({}) {} => {}'.format(roman, n, result))
-#     return result
-#
-# for i in range(37):
-#     a.append(checkio(i))
-#
-# print(a)
-# word = 'A-22-XXX'
-# try:
-#     if word.split('-')[0] not in first_letter:
-#         print(f'Неверное первое слово {word.split("-")[0]}')
-# except Exception:
-#     pass
-# try:
-#     if word.split('-')[1] not in second_letter and word.split('-')[1] is not None:
-#         print(f'Неверное второе слово {word.split("-")[1]}')
-# except Exception:
-#     pass
-# try:
-#     if word.split('-')[2] not in third_letter and word.split('-')[2] is not None:
-#         print(f'Неверное третье слово {word.split("-")[2]}')
-# except Exception:
-#     pass
-# try:
-#     if word.split('-')[3] not in fourth_letter and word.split('-')[3] is not None:
-#         print(f'Неверное четвертое слово {word.split("-")[3]}')
-# except Exception:
-#     pass
-# try:
-#     if word.split('-')[4] not in fifth_letter and word.split('-')[4] is not None:
-#         print(f'Неверное пятое слово {word.split("-")[4]}')
-# except Exception:
-#     pass
 
-potential_nomenclature = ['N-22,23-l', '--', '_-', '4-44—107-—-3', '44-84', '1500-1900', 'Becouc-yeanak',
-                          'l4—44-107', 'M-44-23-[-6', 'N-44-1', 'N-44-1', 'N-45', 'XX-XxX', 'А-А',
+def save_logging(str=None):
+    pass
+
+
+potential_nomenclature = ['N-22,23-1,2', '--', '_-', '4-44—107-—-3', '44-84', '1500-1900', 'Becouc-yeanak',
+                          'l4—44-107', 'M-44,45', 'N-44-1', 'N-44-1', 'N-45', 'XX-XxX', 'А-А',
                           'буро-взрывным', 'Р-ы', '', '-']
+print(potential_nomenclature)
+# Удаляем некорректные слова
 copy_potential_nomenclature = list(potential_nomenclature)
-
+go_next = False
 for word in potential_nomenclature:
     go_next = False
     word_splitted = word.split('-')
@@ -132,24 +77,24 @@ for word in potential_nomenclature:
 
         if word_splitted[0] not in first_letter:
             print(f'{word} Неверное первое слово {word_splitted[0]}')
-            # save_logging(str=f'{word} Неверное первое слово {word_splitted[0]}')
+            save_logging(str=f'{word} Неверное первое слово {word_splitted[0]}')
             copy_potential_nomenclature.remove(word)
             continue
     except Exception:
         pass
     try:
-        # if ',' in word_splitted[1]:
-        #     word2_splitted = word_splitted[1].split(',')
-        #     for w in word2_splitted:
-        #         if w not in second_letter:
-        #             print(f'{word} Неверное второе слово {word_splitted[1]}')
-        #             # save_logging(str=f'{word} Неверное второе слово {word_splitted[1]}')
-        #             copy_potential_nomenclature.remove(word)
-        #             go_next = True
-        #             break
-        # if go_next:
-        #     continue
-        if word_splitted[1] not in second_letter:
+        if ',' in word_splitted[1]:
+            word2_splitted = word_splitted[1].split(',')
+            for w in word2_splitted:
+                if w not in second_letter:
+                    print(f'{word} Неверное второе слово (,) {word_splitted[1]}')
+                    save_logging(str=f'{word} Неверное второе слово {word_splitted[1]}')
+                    copy_potential_nomenclature.remove(word)
+                    go_next = True
+                    break
+        if go_next:
+            continue
+        if ',' not in word_splitted[1] and word_splitted[1] not in second_letter:
             print(f'{word} Неверное второе слово {word_splitted[1]}')
             # save_logging(str=f'{word} Неверное второе слово {word_splitted[1]}')
             copy_potential_nomenclature.remove(word)
@@ -157,23 +102,21 @@ for word in potential_nomenclature:
     except Exception:
         pass
     try:
-        # if ',' in word_splitted[2]:
-        #     word3_splitted = word_splitted[2].split(',')
-        #     for w in word3_splitted:
-        #         if w not in third_letter:
-        #             log = f'{word} Неверное третье слово {word_splitted[2]}'
-        #             print(log)
-        #             # save_logging(str=log)
-        #             copy_potential_nomenclature.remove(word)
-        #             go_next = True
-        #             break
-        # if go_next:
-        #     continue
-        if word.split('-')[2] not in third_letter and word.split('-')[2] is not None:
+        if ',' in word_splitted[2]:
+            word3_splitted = word_splitted[2].split(',')
+            for w in word3_splitted:
+                if w not in third_letter:
+                    print(f'{word} Неверное третье слово (,) {word_splitted[2]}')
+                    save_logging(str=f'{word} Неверное третье слово {word_splitted[2]}')
+                    copy_potential_nomenclature.remove(word)
+                    go_next = True
+                    break
+        if go_next:
+            continue
+        if ',' not in word_splitted[2] and word_splitted[2] not in third_letter:
             log = f'{word} неверное третье слово {word_splitted[2]}'
             print(log)
-            # save_logging(str=log)
-            # save_logging(str=f'неверное третье слово')
+            save_logging(str=log)
             copy_potential_nomenclature.remove(word)
             continue
     except Exception:
